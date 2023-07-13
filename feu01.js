d581d00305a1540dd3b0cc470db76a2ebbd3d4b3
Simston <stephane.simon51@gmail.com>
2023-07-13 20:22:03 +0200

    SecondV

--- feu01.js
+++ feu01.js
@@ -1,16 +1,31 @@
-const userString = "4 + 21 * (2 * 2 / 2) + (38)";
+const userString = "-4 + 21 * (3 - 2 * 6) + 38";
 
 const evaluateExpression = (string) => {
+    // Remplacer les nombres négatifs par une forme reconnaissable
     let resultArray = string.match(/[\d.]+|\+|\*|-|\/|%|\(|\)/g);
+    resultArray = fixNegativeNumbers(resultArray);
     firstTreatment(resultArray);
 };
+
+const fixNegativeNumbers = (array) => {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === "-" && (i === 0 || isOperator(array[i - 1]))) {
+            array.splice(i, 2, "-" + array[i + 1]);
+        }
+    }
+    return array;
+};
+
+const isOperator = (str) => {
+    return ["+", "*", "/", "%", "(", ")"].includes(str);
+};
+
 
 // Traitement dans un premier temps des Parenthèses faire le calcul
 // et renvoyer le resultat pour remplacer les parethèses par le resultat calculer
 const firstTreatment = (array) => {
     //let indexParenthesis = [];
     let i = -1;
-    let cutIndex = 0;
     let extractOperation = [];
 
     while ((i = array.indexOf("(", i + 1)) >= 0) {
@@ -18,20 +33,23 @@ const firstTreatment = (array) => {
         let secondParenthesis = array.indexOf(")", i + 1);
         extractOperation.push(array.slice(i + 1, secondParenthesis));
         //Faire le calcul ici a la place operation
-        calculate(array.slice(i + 1, secondParenthesis));
-        array.splice(i, secondParenthesis - i + 1, "operation");
+        
+        array.splice(i, secondParenthesis - i + 1, calculate(array.slice(i + 1, secondParenthesis)));
+
+        
         //indexParenthesis.push(array.indexOf(")", i+1));
     }
     //console.log(indexParenthesis)
     //console.log(extractOperation)
-    //console.log(array)
+    console.log(calculate(array))
 };
 
 const calculate = (arrayOfOperation) => {
     console.log(arrayOfOperation);
+    let i =0;
 
     while (arrayOfOperation.length > 1) {
-        for (let i = 0; i < arrayOfOperation.length; i++) {
+        for (i = 0; i < arrayOfOperation.length; i++) {
             switch (arrayOfOperation[i]) {
                 case "*":
                     let multiply =
@@ -50,8 +68,23 @@ const calculate = (arrayOfOperation) => {
                     break;
             }
         }
+
+        for (i = 0; i < arrayOfOperation.length; i++) {
+            switch (arrayOfOperation[i]) {
+                case "+":
+                    let add =
+                        Number(arrayOfOperation[i - 1]) + Number(arrayOfOperation[i + 1]);
+                    arrayOfOperation.splice(i - 1, 3, add.toString());
+                    break;
+                case "-":
+                    let sous =
+                        Number(arrayOfOperation[i - 1]) - Number(arrayOfOperation[i + 1]);
+                    arrayOfOperation.splice(i - 1, 3, sous.toString());
+                    break;
+        }
     }
-    console.log(arrayOfOperation);
+}
+    return arrayOfOperation[0];
 };
 
 evaluateExpression(userString);
