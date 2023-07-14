a9670c739ac70f9b0b48f77655d89232c829b5ba
Simston <stephane.simon51@gmail.com>
2023-07-14 14:04:55 +0200

    feu01.js second version

--- feu01.js
+++ feu01.js
@@ -1,47 +1,24 @@
-const userString = "-4 + 21 * (3 - 2 * 6) + 38";
+const userString = "-2 - 4 * (7 - 8 * 8 + 3) + 7 / 7";
 
 const evaluateExpression = (string) => {
     // Remplacer les nombres négatifs par une forme reconnaissable
-    let resultArray = string.match(/[\d.]+|\+|\*|-|\/|%|\(|\)/g);
-    resultArray = fixNegativeNumbers(resultArray);
-    firstTreatment(resultArray);
+    let resultArray =   string.match(/(-?\d+|\+|\*|\/|%|\(|\)|-)/g);
+    console.log(firstTreatment(resultArray));
 };
-
-const fixNegativeNumbers = (array) => {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === "-" && (i === 0 || isOperator(array[i - 1]))) {
-            array.splice(i, 2, "-" + array[i + 1]);
-        }
-    }
-    return array;
-};
-
-const isOperator = (str) => {
-    return ["+", "*", "/", "%", "(", ")"].includes(str);
-};
-
 
 // Traitement dans un premier temps des Parenthèses faire le calcul
 // et renvoyer le resultat pour remplacer les parethèses par le resultat calculer
 const firstTreatment = (array) => {
-    //let indexParenthesis = [];
     let i = -1;
     let extractOperation = [];
 
     while ((i = array.indexOf("(", i + 1)) >= 0) {
-        //indexParenthesis.push(i);
         let secondParenthesis = array.indexOf(")", i + 1);
         extractOperation.push(array.slice(i + 1, secondParenthesis));
         //Faire le calcul ici a la place operation
-        
         array.splice(i, secondParenthesis - i + 1, calculate(array.slice(i + 1, secondParenthesis)));
-
-        
-        //indexParenthesis.push(array.indexOf(")", i+1));
     }
-    //console.log(indexParenthesis)
-    //console.log(extractOperation)
-    console.log(calculate(array))
+    return calculate(array);
 };
 
 const calculate = (arrayOfOperation) => {
