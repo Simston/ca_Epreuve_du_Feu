aaa5343d7e417f3cc10778b567a40dc194b0fc41
Simston <stephane.simon51@gmail.com>
2023-07-19 19:18:52 +0200

    update

--- feu02.js
+++ feu02.js
@@ -1,11 +1,13 @@
 let tab1 = [[0, 0, 0, 0], [1, 1, 1, 1], [2, 3, 3, 1]];
-let tab2 = [[1, 1], [1]];
+let tab2 = [[1, 1], ["-", 1]];
 let tabResult = [];
 
 
 let formWidth = tab2[0].length;
 let formHeight = tab2.length;
 let count = 0;
+let found = false;
+let indexK = 0;
 
 const findForm = () => {
     for (let i = 0; i < tab1.length; i++) {
@@ -14,33 +16,39 @@ const findForm = () => {
         console.log(tab1[i] + "Premiere boucle")
 
         for (let j = 0; j < tab1[i].length; j++) {
-            let found = false;
-            let indexesCompare = 0; // Utilisation d'un tableau pour stocker les index correspondants
+            found = false;
 
             for (let k = 0; k < formHeight; k++) {
+                indexK = k;
                 for (let l = 0; l < tab2[k].length; l++) {
-                    if (found && formWidth === count) {
-                        console.log("TOTO")
-                        console.log(count)
-                        if (tab1[i + 1][l] === tab2[k][l]) {
-                            console.log(tab1[i + 1][l] + "brolala");
-                            count = 0;
-                        } else {
-                            found = false;
-                        }
-                    } else if (tab1[i][j] === tab2[k][l]) {
+                    console.log(tabCompare)
+                    if (tab1[i][j] === tab2[k][l]) {
                         found = true;
-                        indexesCompare = j; // Stocker l'index correspondant dans le tableau indexesCompare
+
                     }
+                }
+                if (found && formWidth === count) {
+
+                    let nextLineOfTab1 = tab1[i + 1];
+                    console.log(nextLineOfTab1)
+                    let nextLineOfTab2 = tab2[indexK + 1];
+                    console.log(nextLineOfTab2)
+                    if (nextLineOfTab1 !== undefined) {
+                        verifyNextLine(nextLineOfTab1, nextLineOfTab2, tabCompare, j);
+
+                    }
+                    console.log(tabCompare + " test retour");
                 }
             }
             //console.log(indexesCompare)
+
             if (found) {
-                tabCompare.push(tab1[i][j]);
                 count += 1;
+                tabCompare.push(tab1[i][j]);
                 console.log(tabCompare)
             } else {
                 tabCompare.push('-');
+                console.log(tabCompare);
             }
         }
 
@@ -50,4 +58,42 @@ const findForm = () => {
     console.log(tabResult);
 
 }
+
+const verifyNextLine = (nextLineA, nextLineB, tabCompare, indexPosJ) => {
+    let tempSecondJ = indexPosJ;
+    console.log(indexPosJ + " JJJJJJJ")
+    indexPosJ = indexPosJ - nextLineB.length;
+    console.log(nextLineB.length + " index pos j")
+    for (let i = 0; i < nextLineB.length; i++) {
+        console.log(nextLineA[i + indexPosJ] + "verif en cours de TAB1 I J");
+        console.log(nextLineB[i])
+        if (nextLineA[i + indexPosJ] !== nextLineB[i] && nextLineB[i] !== "-") {
+            count = 0;
+            found = false;
+            console.log(tabCompare + "  TABCOMPARE")
+            tabCompare = resetTab(tabCompare, tempSecondJ);
+            console.log("BRO")
+        } else {
+            console.log("LALA")
+
+            found = true;
+        }
+    }
+    if (found) {
+        return found;
+    } else {
+        return tabCompare
+    }
+}
+
+const resetTab = (array, indexJ) => {
+
+    for (let i = 0; i < indexJ; i++) {
+        array.splice(i, 1, "0");
+    }
+    console.log(array + " array")
+    return array;
+}
+
+
 findForm();
